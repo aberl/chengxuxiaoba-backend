d66829e2cb901240ee653975c6cd936dd789ce95
aberl <aberl-workload@163.com>
2019-06-24 22:28:38 +0800

    add role payment list

--- video/src/main/java/com/chengxuxiaoba/video/model/Response/VO/RolePaymentResponseVo.java
+++ video/src/main/java/com/chengxuxiaoba/video/model/Response/VO/RolePaymentResponseVo.java
@@ -1,6 +1,9 @@
 package com.chengxuxiaoba.video.model.Response.VO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class RolePaymentResponseVo {
     private Integer id;
@@ -9,6 +12,10 @@ public class RolePaymentResponseVo {
     private Integer roleId;
     private BigDecimal price;
     private Integer status;
+    @JsonFormat(pattern="yyyy/MM/dd HH:mm:ss")
+    private Date startDate;
+    @JsonFormat(pattern="yyyy/MM/dd HH:mm:ss")
+    private Date endDate;
 
     public Integer getId() {
         return id;
@@ -56,5 +63,21 @@ public class RolePaymentResponseVo {
 
     public void setStatus(Integer status) {
         this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
 }
